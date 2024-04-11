fe67b8b5626281d8d37315818c249e6012d186a6
niccolobuonfiglio <166041610+niccolobuonfiglio@users.noreply.github.com>
2024-04-11 15:49:35 +0200

    Update registrati.js

--- javascript/registrati.js
+++ javascript/registrati.js
@@ -7,6 +7,10 @@ function validaForm(){
     var email = document.getElementById("email").value; 
     var cpassword = document.getElementById("cpassword").value;
     var regexLiteral = /[A-z0-9\.\+_-]+@[A-z0-9\._-]+\.[A-z]{2,6}/;
+    var dataObj = document.getElementById("data").value;
+    var annoCorrente = dataObj.getFullYear();
+    var annoMinimo = annoCorrente - 16;
+    var annoMassimo = annoCorrente - 100;
     // rendere obbligatori anche data nascita e sesso
     
     if(username =="" || password=="" || nome=="" || cognome=="" || email==""){
@@ -23,6 +27,10 @@ function validaForm(){
     }
     if(!regexLiteral.test(document.registr.email.value)){
         alert("Inserisci un indirizzo email valido");
+        return false;
+    }
+    if(dataObj.getFullYear() < annoMassimo || dataObj.getFullYear() > annoMinimo){
+        alert("Inserisci una data di nascita valida");
         return false;
     }
     return true;

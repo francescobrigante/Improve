20d5b4f61c772d01e31a09f0a8a57ae7af31d3dc
AlessioCalcagni0 <161961913+AlessioCalcagni0@users.noreply.github.com>
2024-04-11 12:13:22 +0200

    Add files via upload

---
+++ registrati.js
@@ -0,0 +1,21 @@
+function validaForm(){
+
+    var username = document.getElementById("username").value; 
+    var password = document.getElementById("password").value; 
+    var cpassword = document.getElementById("cpassword").value;
+    
+    if(username || password ){
+        alert("Inserisci username e password");
+        return false;
+    }
+    if(password < 8){
+        alert("La password deve essere di almeno 8 caratteri");
+        return false;
+    }
+    if(password!=cpassword){
+        alert("Le password non coincidono");
+        return false;
+    }
+    return true;
+}
+

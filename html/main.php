20724a77d57f35eac97c5606680a7bd4fd2a07ff
francescobrigante <francescobrigantefb@gmail.com>
2024-04-28 15:52:25 +0200

    fetch modifiche

--- html/main.php
+++ html/main.php
@@ -17,6 +17,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Improve</title>
         <link rel="stylesheet" type="text/css" href="../css/main.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
         <script src="//code.jquery.com/jquery-3.7.1.js"></script>
         <script src="../javascript/main.js"></script>
     </head>
@@ -55,9 +56,79 @@
 
     <!-- barra secondaria -->
     <div class="barra2">
-        <button class="nuova_scheda">+</button>
+            <button class="nuovascheda" onmouseover="changeToNuovaScheda()" onmouseout="changeToPlus()">+</button>
+            <div class="search-bar">
+                <input type="text" placeholder="Cerca un esercizio..." > 
+                <button> <i class="fas fa-search" ></i></button>
+            </div>
+            <button class="filter" onclick="FilterAnimation()"> <i id ="bars" class="fa-solid fa-bars" ></i></button>
+        </div>
+        <div class="contenitore" >
+        <ul class="list-items" id="list">
+            <li class="item" onclick="CheckBoxPetto()">
+                <span class="checkbox">
+                    <i class="fa-solid fa-check check-icon"  id="Petto"></i>
+                </span>
+                <span class="item-text">Petto</span>
+            </li>
+            <li class="item" onclick="CheckBoxSpalle()">
+                <span class="checkbox">
+                    <i class="fa-solid fa-check check-icon" id="Spalle"></i>
+                </span>
+                <span class="item-text">Spalle</span>
+            </li>
+            <li class="item" onclick="CheckBoxTricipiti()">
+                <span class="checkbox" >
+                    <i class="fa-solid fa-check check-icon" id="Tricipiti"></i>
+                </span>
+                <span class="item-text">Tricipiti</span>
+            </li>
+            <li class="item" onclick="CheckBoxAddominali()" >
+                <span class="checkbox" >
+                    <i class="fa-solid fa-check check-icon" id="Addominali"></i>
+                </span>
+                <span class="item-text">Addominali</span>
+            </li>
+            <li class="item" onclick="CheckBoxBicipiti()">
+                <span class="checkbox" >
+                    <i class="fa-solid fa-check check-icon" id="Bicipiti"></i>
+                </span>
+                <span class="item-text">Bicipiti</span>
+            </li>
+            <li class="item" onclick="CheckBoxGambe()">
+                <span class="checkbox" > 
+                    <i class="fa-solid fa-check check-icon" id="Gambe"></i>
+                </span>
+                <span class="item-text">Gambe</span>
+            </li>
+        </ul>
     </div>
 
+    <div class="scrollbox" >
+        <button class ="box-exercise" onclick="openPopup()" >
+            <h1>Panca piana</h1>
+            <h2>Petto</h2>
+            <img src="../fotoesercizi/Pettorali/Screenshot_2024.03.30_16.55.31.055.png" id="petto">
+        </button>
+        <button class ="box-exercise">
+            <h1>Chest Press</h1>
+            <h2>Petto</h2>
+            <img src="../fotoesercizi/Pettorali/Screenshot_2024.03.30_16.56.31.822.png" id="petto">
+        </button>
+    </div>
 
+    <div class="popup" id="popup">
+       <h1> Panca Piana </h1>
+       <h2> Petto </h2>
+       <img src="../fotoesercizi/Pettorali/Screenshot_2024.03.30_16.55.31.055.png" >
+       <h3>Descrizione</h3>
+       <div class="description"> Posizione iniziale: sdraiarsi sul tappetino con la schiena a terra. Piegare le gambe, tenendo i piedi appogiati a terra ed intrecciare le mani dietro la nuca.
+        Movimento: flettere il busto in avanti, sollevando dal tappetino a parte superiore del busto.
+        Scopo esercizio: esercizio a corpo libero molto comune che concentra il lavoro sulla sezione centrale dell’addome (retto dell’addome) ed in parte anche a livello dei fianchi (obliqui). Viene solitamente effettuato con un alto numero di ripetizioni.
+        Respirazione: inspirare abbassando il busto sul tappetino ed espirare flettendolo.
+        Errori: sollevare dal tappetino anche la parte bassa della schiena, far forza con le braccia per aiutarsi a flettere il busto e sollevare i piedi da terra. </div>
+    </div>
+
+    <div id="overlay" class="overlay" onclick="closePopup()"> </div>
 </body>
 </html>
--- javascript/main.js
+++ javascript/main.js
@@ -40,3 +40,115 @@ $(document).ready(function(){
     });
 
 });
+
+// Funzione per cambiare il testo del bottone a "Nuova Scheda"
+function changeToNuovaScheda() {
+    const button = document.querySelector('.nuovascheda');
+    button.textContent = 'Nuova scheda';
+}
+
+// Funzione per cambiare il testo del bottone a "+"
+function changeToPlus() {
+    const button = document.querySelector('.nuovascheda');
+    button.textContent = '+';
+}
+
+function openPopup(){
+    document.getElementById("popup").classList.add("popupactive")
+    document.getElementById("overlay").classList.add("overlayactive");
+}
+
+function closePopup(){
+    document.getElementById("popup").classList.remove("popupactive")
+    document.getElementById("overlay").classList.remove("overlayactive");
+}
+
+let clickcount = 0 ; 
+function FilterAnimation(){
+
+        if ( clickcount % 2 === 0 ){
+        // Primo click: Esegui animazione 1
+        document.getElementById("bars").classList.add("rotatebars");
+        document.getElementById("list").classList.add("list-items-open");
+        }
+        // Secondo click: Esegui animazione 2
+        else{ 
+        document.getElementById("bars").classList.remove("rotatebars");
+        document.getElementById("list").classList.remove("list-items-open");
+
+            
+        }
+        clickcount++;
+}
+
+let counterPetto = 0 ;
+function CheckBoxPetto(){
+
+    if (counterPetto % 2 === 0 ){
+        document.getElementById("Petto").classList.add("check-icon-checked");
+    }
+    else{ 
+        document.getElementById("Petto").classList.remove("check-icon-checked");
+    }
+    counterPetto++;
+}
+
+let counterSpalle = 0 ;
+function CheckBoxSpalle(){
+
+    if (counterSpalle % 2 === 0 ){
+        document.getElementById("Spalle").classList.add("check-icon-checked");
+    }
+    else{ 
+        document.getElementById("Spalle").classList.remove("check-icon-checked");
+    }
+    counterSpalle++;
+}
+
+let counterTricipiti = 0 ;
+function CheckBoxTricipiti(){
+
+    if (counterTricipiti % 2 === 0 ){
+        document.getElementById("Tricipiti").classList.add("check-icon-checked");
+    }
+    else{ 
+        document.getElementById("Tricipiti").classList.remove("check-icon-checked");
+    }
+    counterTricipiti++;
+}
+
+let counterAddominali = 0 ;
+function CheckBoxAddominali(){
+
+    if (counterAddominali % 2 === 0 ){
+        document.getElementById("Addominali").classList.add("check-icon-checked");
+    }
+    else{ 
+        document.getElementById("Addominali").classList.remove("check-icon-checked");
+    }
+    counterAddominali++;
+}
+
+let counterBicipiti = 0 ;
+function CheckBoxBicipiti(){
+
+    if (counterBicipiti % 2 === 0 ){
+        document.getElementById("Bicipiti").classList.add("check-icon-checked");
+    }
+    else{ 
+        document.getElementById("Bicipiti").classList.remove("check-icon-checked");
+    }
+    counterBicipiti++;
+}
+
+let counterGambe = 0 ;
+function CheckBoxGambe(){
+
+    if (counterGambe % 2 === 0 ){
+        document.getElementById("Gambe").classList.add("check-icon-checked");
+    }
+    else{ 
+        document.getElementById("Gambe").classList.remove("check-icon-checked");
+    }
+    counterGambe++;
+}
